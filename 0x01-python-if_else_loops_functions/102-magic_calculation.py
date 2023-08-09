afcab0c002babc597ec23c7f0dd849209acbaa3e
Rafiki Blaise <rafikiblaise@gmail.com>
2023-08-09 04:34:04 -0700

    102-magic_calculation.py

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -6,7 +6,7 @@ def magic_calculation(a, b, c):
     """Match bytecode provided by Holberton School."""
     if a < b:
         return (c)
-    if c > b:
+    elif c > b:
         return (a)
     if c < a:
         return (b)
